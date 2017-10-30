0b0878525299ec97fdd233a2929af020f34e5d45
zhaopeng <dbird1995@163.com>
2017-10-30 14:18:52 +0800

    config_modify

---
+++ function/deploy/export_config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#-----------------------------------------------------------------
+# Filename:    install.sh
+# Version:    2.2.1
+# Date:        2017/10/30
+# Author:      ZhaoPeng
+# Description: 
+#-----------------------------------------------------------------
+# version 2.1   add  logging function
+# version 2.2.1  update and deploy  combined
+
+workspace=`pwd`
+app_check=${workspace}/tmp/acmupdate/info/app_check.txt
+rm -rf ${app_check} && touch ${app_check}
+config_files=("ares-app-config.xml" "server.properties" "log4j.properties")
+# 获取app_name ，过滤备份目录
+for app_name in `ls  ${workspace}/deploy|egrep [a-zA-Z]|egrep -v [0-9]$|egrep -v bak$`
+do
+    # app_check
+    app_path=${workspace}/deploy/${app_name}
+    tom_path=${workspace}/tomcats/tomcat-${app_name}
+    echo ${app_name}"|"${app_path}"|"${tom_path} >> ${app_check}
+    shell_log "[${app_name}] checked....app_path: ${app_path} , tom_path: ${tom_path}  "  # log
+    log_path=${workspace}/logs/${app_name}
+    sed -i "\?${app_name}? s?\$?|${log_path}?" ${app_check}
+
+    # get_conf
+    config_path="/share/update_deploy_wars/conf/${app_name}/${ip_addr}"
+    [[ ! -d ${config_path} ]] &&   mkdir -p ${config_path}
+    for config_file in  `echo ${config_files[*]}`
+    do
+        [ -f ${app_path}/WEB-INF/classes/$config_file ]  && cp -p  ${app_path}/WEB-INF/classes/$config_file ${config_path}
+        [ -f ${app_path}/WEB-INF/conf/$config_file ]  && cp  -p ${app_path}/WEB-INF/conf/$config_file ${config_path}
+    done
+    
+done
+
+
+
+
